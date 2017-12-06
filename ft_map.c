90362315aeaed69635d6fe3ec17e47dcebfa1fb6
Coralie Eugene <baeth@mbp-de-coralie.home>
2017-12-06 17:10:08 +0100

    ft_map a la norme

--- ft_map.c
+++ ft_map.c
@@ -173,22 +173,16 @@ int		ft_map(t_rex **start)
 
 	i = 0;
 	first = *start;
-	if (first->h >= first->l)
-		size = first->h;
-	else
-		size = first->l;
+	first->h >= first->l ? (size = first->h) : (size = first->l);
 	if (!(map = (char **)malloc(sizeof(char *) * size + 1)))
 		return (1);
 	while (i < size)
 	{
-		select = 0;
+		select = -1;
 		if (!(map[i] = (char *)malloc(sizeof(char) * size + 1)))
 			return (1);
-		while (select < size)
-		{
+		while (++select < size)
 			map[i][select] = '.';
-			select++;
-		}
 		map[i][select] = '\0';
 		i++;
 	}
